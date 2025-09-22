c83bb662d7d2df3323b91ce82c424d09ddc8d0e7
vlad907 <vladimiravdeev@MacBook-Air.local>
2025-09-22 13:45:45 -0700

    next config fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",             // Required for static export
-  images: { unoptimized: true },// Disable image optimizer for GitHub Pages
+  images: {
+    unoptimized: true,          // Disable image optimizer for GitHub Pages
+  },
   trailingSlash: true,          // Ensures proper routing on Pages
   basePath: "/Blue-arc",
   assetPrefix: "/Blue-arc/",
